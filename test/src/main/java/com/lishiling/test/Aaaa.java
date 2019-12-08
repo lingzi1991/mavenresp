d113b9a9b29bf3beaf37e4c3942bc0504da881e7
李世领 <lishiling@yixia.con>
2019-12-08 15:32:06 +0800

    当时发生的

--- test/src/main/java/com/lishiling/test/Aaaa.java
+++ test/src/main/java/com/lishiling/test/Aaaa.java
@@ -6,4 +6,8 @@ public class Aaaa {
     public static void LogTxt(String s){
         Log.e("Tag", "---------"  + s);
     }
+
+    public static void LogTxt2(String s){
+        Log.e("Tag", "---------"  + s);
+    }
 }
